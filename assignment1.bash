4c290eb165bd5af8ee109e6ef112bc7b03cad541
Matthew Lewis <matt@mplewis.com>
2013-02-10 15:12:23 -0600

    first commit

---
+++ assignment1.bash
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+#---------------------------------------------------------------------
+# CSci 4061 Spring 2013
+# Assignment# 1
+# Student name: Matthew Lewis, Chris Drews
+# Student id: 4237332, id2
+# x500 id: lewi0486, id2
+# Operating system on which you tested your code: Linux, Unix, Solaris
+# CSELABS machine: <machine you tested on eg: xyz.cselabs.umn.edu>
+#---------------------------------------------------------------------
+
+echo " "
+
+######################
+# Directives for usage
+
+  output="SELECT THE FUNCTION YOU WANT TO EXECUTE: \n" 
+  echo -e $output
+  output="1. Search for files" 
+  echo -e $output
+  output="2. Calculate total length" 
+  echo -e $output
+  output="3. Find zero length files" 
+  echo -e $output
+  output="4. Create backup files" 
+  echo -e $output
+  output="5. Exit" 
+  echo -e $output
+       
+
+  echo -e "\nEnter option "
+  read option
+
+#  echo $option 
+  
+  case "$option" in
+
+  1)  echo "Searching for files"
+      #here you should implement for search files
+      # Begin Code
+      
+      #End Code
+      ;;
+  2)  echo "Calculating total of the size of all files in the directory tree"
+      #here you should implement the code to calculate the size of all files in a folder
+      # Begin Code
+      
+      #End Code
+      ;;
+      
+  3) echo "Finding zero length files"
+      #here you should implement the code to find empty files
+      echo -e "\nEnter directory name: "
+      read dirname
+      find $dirname -size 0
+      #End Code
+      ;;
+      
+   4) echo  "Creating backup files"
+      #here you should implement the backup code  
+      # Begin Code
+      
+      #End Code
+      ;;
+      
+  5) echo "Exiting the program. Thanks"
+    exit
+    ;;
+   esac
